945041e34e420fc9f0a7096a66164da0238f140d
Celticcow <gregory.dunlap@gmail.com>
2020-06-12 07:13:25 -0500

    starter files for lock check

---
+++ islocked.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 -W ignore::DeprecationWarning
+
+import requests
+import json
+import sys
+import time
+import argparse
+import apifunctions
+
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+"""
+gregory.dunlap / celtic_cow
+
+"""
+
